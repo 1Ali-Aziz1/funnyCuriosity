a1d875c4c2de3d7e8475972e7733b69d3f2856e9
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-08-12 17:24:35 +0530

    DONE!

---
+++ Collatz Conjecture.py
@@ -0,0 +1,30 @@
+def end(greatestNumber, steps):
+    print("Greatest number: " + str(greatestNumber))
+    print("Total steps taken: " + str( steps-1))
+    quit()
+    # raise Exception("cause of the problem")
+num = int(input("Enter the number: "))
+
+
+def fun(n, greatestNumber, steps):
+    steps = steps + 1
+    if n > greatestNumber:
+        greatestNumber = n
+    if (n == 1):
+        end(greatestNumber, steps)
+    #checking if the number is even
+    if (n % 2) == 0:
+        n2 = n/2
+        fun(n2, greatestNumber, steps)
+    #checking if the number is odd
+    n3 = n/2
+    if (isinstance(n3, float)  ):
+        n1 = (3 * n) + 1
+        fun(n1, greatestNumber, steps)
+
+global greatestNumber 
+greatestNumber = 0
+global steps
+steps = 0
+fun(num, greatestNumber, steps)
+        
