ffd5a180a498dd038990df2b82be912606b1a91e
1Ali-Aziz1 <dr.aliaziz45@gmail.com>
2024-08-18 19:33:39 +0530

    Get lazy, get done. Just doing this because I coded on another machine

--- Collatz Conecture(Graphing).py
+++ Collatz Conecture(Graphing).py
@@ -7,9 +7,11 @@ def fun(n, greatestNumber, steps):
     #checking if the number is even
     if (n % 2) == 0:
         n2 = n/2
-        fun(n2, greatestNumber, steps)
+        return fun(n2, greatestNumber, steps)
     #checking if the number is odd
     n3 = n/2
     if (isinstance(n3, float)  ):
         n1 = (3 * n) + 1
-        fun(n1, greatestNumber, steps)
+        return fun(n1, greatestNumber, steps)
+    
+
